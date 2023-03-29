94de8eae6d58c499905c1fcb0485531a89aaa486
AlonMesh <97172662+AlonMesh@users.noreply.github.com>
2023-03-29 15:48:28 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -6,26 +6,26 @@
 #include <string>
 
 TEST_CASE("The wished player has been created") {
-    ariel::Player player("Avner");
-    CHECK(player.getName() == "Avner");
+    ariel::Player player("Alice");
+    CHECK(player.getName() == "Alice");
 }
 
 TEST_CASE("Initial STACK SIZE of player before entering a game is 0") { 
-    ariel::Player player("Avner");
+    ariel::Player player("Alice");
     CHECK(player.stacksize() == 0);
 }
 
 TEST_CASE("Initial CARDS TAKEN of player before entering a game is 0") { 
-    ariel::Player player("Avner");
+    ariel::Player player("Alice");
     CHECK(player.cardesTaken() == 0);
 }
 
 TEST_CASE("STACK SIZE is always in the range (0,26) included") { 
-    ariel::Player p1("Avner");
+    ariel::Player p1("Alice");
     CHECK(p1.stacksize() >= 0);
     CHECK(p1.stacksize() <= 26);
 
-    ariel::Player p2("Benny");
+    ariel::Player p2("Bob");
     ariel::Game game(p1, p2);
 
     for (int i = 0; i < 5; i++)
@@ -37,11 +37,11 @@ TEST_CASE("STACK SIZE is always in the range (0,26) included") {
 }
 
 TEST_CASE("CARDS TAKEN is always in the range (0,26) included") {
-    ariel::Player p1("Avner");
+    ariel::Player p1("Alice");
     CHECK(p1.cardesTaken() >= 0);
     CHECK(p1.cardesTaken() <= 26);
 
-    ariel::Player p2("Benny");
+    ariel::Player p2("Bob");
     ariel::Game game(p1, p2);
 
     for (int i = 0; i < 5; i++)
@@ -53,12 +53,25 @@ TEST_CASE("CARDS TAKEN is always in the range (0,26) included") {
 }
 
 TEST_CASE("The wished players are fields of a given game") {
-    ariel::Player p1("Avner");
-    ariel::Player p2("Benny");
+    ariel::Player p1("Alice");
+    ariel::Player p2("Bob");
     ariel::Game game(p1, p2);
 
     CHECK(game.getPlayer1().getName() == p1.getName());
     CHECK(game.getPlayer2().getName() == p2.getName());
+}
+
+TEST_CASE("Player can play in an unique game") {
+    ariel::Player p1("Alice");
+    ariel::Player p2("Bob");
+    ariel::Game game(p1, p2);
+
+    ariel::Player p3("Charlie");
+    ariel::Game game2(p1, p3);
+    CHECK_THROWS(game2.playTurn());
+    CHECK_THROWS(game2.playAll());
+    CHECK_THROWS(game2.getPlayer1());
+    CHECK_THROWS(game2.getPlayer2());
 }
 
 TEST_CASE("When the game begins the deck is shuffled correctly - each player gets half of the cards") {
@@ -90,40 +103,6 @@ TEST_CASE("After each turn there are still 52 cards") {
     CHECK((p1.cardesTaken() + p1.stacksize() + p2.cardesTaken() + p2.stacksize()) == 52);
     game.playTurn();
     CHECK((p1.cardesTaken() + p1.stacksize() + p2.cardesTaken() + p2.stacksize()) == 52);
-}
-
-TEST_CASE("PlayTurn() reduces 1 or 3 cards of each STACK SIZE") {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
-
-    for (int i = 0; i < 3; i++)
-    {
-        int originalSize = p1.stacksize();
-        game.playTurn();
-        bool cond1 = originalSize - 1 == p1.stacksize();
-        bool cond2 = originalSize - 3 == p1.stacksize();
-        bool finalBool = cond1 || cond2;
-        CHECK(finalBool);
-    }
-}
-
-TEST_CASE("PlayTurn() grants 2 or 6 cards to specific CARDS TAKEN") { // original 1 or 3 and the other 1 or 3
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
-
-    for (int i = 0; i < 3; i++)
-    {
-        int originalSize1 = p1.cardesTaken();
-        int originalSize2 = p2.cardesTaken();
-        game.playTurn();
-        bool cond1 = (originalSize1 + 2 == p1.cardesTaken() && originalSize2 == p2.cardesTaken());
-        bool cond2 = (originalSize1 + 6 == p1.cardesTaken() && originalSize2 == p2.cardesTaken());
-        bool cond3 = (originalSize2 + 2 == p2.cardesTaken() && originalSize1 == p1.cardesTaken());
-        bool cond4 = (originalSize2 + 6 == p2.cardesTaken() && originalSize1 == p1.cardesTaken());
-        CHECK((cond1 || cond2 || cond3 || cond4));
-    }
 }
 
 TEST_CASE("After the whole game there are still 52 cards") {
